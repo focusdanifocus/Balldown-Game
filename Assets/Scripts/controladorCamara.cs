823c7f499f99e14fcefbb429851efb4fe838fb44
Daniel Franco <focusdanifocus@gmail.com>
2020-08-04 12:12:30 -0400

    primer commit

---
+++ Assets/Scripts/controladorCamara.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controladorCamara : MonoBehaviour
+{
+    public GameObject jugador;
+    Vector3 posicionRelativa;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        posicionRelativa = transform.position - jugador.transform.position;
+       
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+         transform.position = jugador.transform.position + posicionRelativa;
+         
+    }
+}
---
+++ Assets/Scripts/controladorJugador.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controladorJugador : MonoBehaviour
+{
+    //Llamar el componente Rigidbody del jugador
+    public Rigidbody rbJugador;
+    public float velocidad;
+  
+    
+
+    
+    public void FixedUpdate()
+    {
+        //CONTROLADOR del jugador
+        float movimientoHorizontal = Input.GetAxis("Horizontal");
+        float movimientoVertical = Input.GetAxis("Vertical");
+        Vector3 movimiento = new Vector3(movimientoHorizontal,0,movimientoVertical);
+        //FISICAS
+        rbJugador.AddForce(movimiento * velocidad);
+
+    }
+}
