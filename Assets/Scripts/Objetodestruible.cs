a39429cf33e75545708484099c9c69f9f87eec23
Daniel Franco <68919112+focusdanifocus@users.noreply.github.com>
2020-08-11 19:37:33 -0400

    Agregue elemento destruible y comienzo de nivel 8

---
+++ Assets/Scripts/Objetodestruible.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objetodestruible : MonoBehaviour
+{
+    public GameObject bloque;
+    
+    public void  OnCollisionEnter() {
+        Destroy(bloque,2f);
+    }
+}
