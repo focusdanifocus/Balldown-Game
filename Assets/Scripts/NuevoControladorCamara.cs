d2d6562624b888b7d9b136e1eb7015768a7ac5b9
Daniel Franco <68919112+focusdanifocus@users.noreply.github.com>
2020-08-15 23:15:55 -0400

    Cambio de Script para la camara, Nuevo script para objeto rotante

---
+++ Assets/Scripts/NuevoControladorCamara.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NuevoControladorCamara : MonoBehaviour
+{
+    public Transform jugador;
+    public float suavizado = 0.125f;
+
+    public Vector3 offset = new Vector3(0,5,0);
+
+   
+    void LateUpdate() {
+        transform.position = jugador.position + offset;
+        
+    }
+}
---
+++ Assets/Scripts/rorateObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rorateObject : MonoBehaviour
+{
+    Vector3 rotation = new Vector3(0,1,0);
+    public float speedRotation;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotation * speedRotation);
+    }
+}
