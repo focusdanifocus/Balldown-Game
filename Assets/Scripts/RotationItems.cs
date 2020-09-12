9068cc0df133c57578119d0084d454a1c3c0215c
Daniel Franco <68919112+focusdanifocus@users.noreply.github.com>
2020-09-12 17:57:26 -0400

    mejoras menores

--- Assets/Scripts/RotationItems.cs
+++ Assets/Scripts/RotationItems.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class RotationItems : MonoBehaviour
 {
+    public float rotationSpeed = 45;
     Vector3 rotationItem = new Vector3(0,45,0);
     
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(rotationItem * Time.deltaTime);
+        transform.Rotate(rotationItem * Time.deltaTime * rotationSpeed);
     }
 }
