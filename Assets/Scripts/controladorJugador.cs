4b848217ebf500c386e2ea9a2e868c1fd3968a61
Daniel Franco <68919112+focusdanifocus@users.noreply.github.com>
2020-09-16 16:42:31 -0400

    Comienzo nivel 16

--- Assets/Scripts/controladorJugador.cs
+++ Assets/Scripts/controladorJugador.cs
@@ -12,6 +12,7 @@ public class controladorJugador : MonoBehaviour
 
     public void Update()
     {
+        //Sales del juego si apretas la tecla Escape
         if (Input.GetKeyDown(KeyCode.Escape)){
             Application.Quit();
         }
@@ -20,17 +21,17 @@ public class controladorJugador : MonoBehaviour
     
     public void FixedUpdate()
     {
-        //CONTROLADOR del jugador
+        //Almacenar el movimiento del analogo o las teclas de dirección
         float movimientoHorizontal = Input.GetAxis("Horizontal");
         float movimientoVertical = Input.GetAxis("Vertical");
         Vector3 movimiento = new Vector3(movimientoHorizontal,0,movimientoVertical);
-        //FISICAS
+        //Añado una fuerza al Rigidbody del jugador
         rbJugador.AddForce(movimiento * velocidad * Time.deltaTime);
          
     }
 
     public void Resetear() 
-    {
+    {   // Resetea el nivel si mueres o pierdes
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
     
