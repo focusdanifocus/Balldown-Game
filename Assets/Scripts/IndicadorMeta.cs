974bb0879654101d27d6e003b5db8578f88cd1bc
Daniel Franco <68919112+focusdanifocus@users.noreply.github.com>
2020-11-12 18:51:15 -0400

    collider's fixes

--- Assets/Scripts/IndicadorMeta.cs
+++ Assets/Scripts/IndicadorMeta.cs
@@ -6,12 +6,24 @@ public class IndicadorMeta : MonoBehaviour
 {
     private Collider indicador;
     public GestorNiveles Gestor;
+    public float waitTimeload;
 
     private void Start() {
         indicador = GetComponent<Collider>();
     }
 
     private void OnTriggerEnter(Collider other) {
-        Gestor.CargarSiguienteNivel();
+        StartCoroutine(waitforLoad());
     }
+
+    IEnumerator waitforLoad()
+    {
+
+    
+        yield return new WaitForSeconds(waitTimeload);
+        Gestor.CargarSiguienteNivel();
+    
+        
+    }
+        
 }
